0afa1225bf19c541b83c9fc3ac198d001e5abc8a
SDW1014 <apif730@gmail.com>
2024-02-23 20:36:06 +0900

    [feat] main folderReadAPI연결,find_now_directory 추가, 주석으로 함수 설명 추가  - find_now_directory와 list_files_in_directory의 설명 추가

--- folderReadAPI.py
+++ folderReadAPI.py
@@ -1,8 +1,13 @@
 import os
 
+#directory_path를 기준으로 그 안에 있는 파일들의 정보를 전부 가져온다. 
 def list_files_in_directory(directory_path):
     entries = os.listdir(directory_path)
     
     files = [entry for entry in entries if os.path.isfile(os.path.join(directory_path, entry))]
     
     return files
+
+#지금 나의 경로를 가져온다. 
+def find_now_directory():
+    return os.getcwd()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import folderReadAPI
 
+directory_path = ''
+
 def main():
-    print("hello")
+    #print("hello")
+    print(folderReadAPI.list_files_in_directory(folderReadAPI.find_now_directory()))
+    
     
 if __name__ == "__main__":
     main()
