000d256605c10ebbf097b99f3371a0f6dcc00ee8
SDW1014 <apif730@gmail.com>
2024-02-24 03:02:30 +0900

    [feat] main.py 개선 - test.mp4 테스트 용

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ import folderReadAPI
 def main():
     ## 기본으로 가져가는 hello 함수 #불필요###
     # print("hello")
+    
     ## folderReadAPI.find_now_directory()를 통해 실행 py의 경로를 확인하고, folderReadAPI.list_files_in_directory를 통해 파일들을 리스트로 가져옴 그리고 print함## 
     # print(folderReadAPI.list_files_in_directory(folderReadAPI.find_now_directory()))
+    
     ##test.txt -> demon.txt -> test.txt##
-    folderReadAPI.set_file_title("test.txt", "demon.txt")
-    folderReadAPI.set_file_title("demon.txt", "test.txt")
+    # folderReadAPI.set_file_title("test.txt", "demon.txt")
+    # folderReadAPI.set_file_title("demon.txt", "test.txt")
+    
+    ## extract_fileextension_checker를 사용한 기능 체크 ##
+    for match in folderReadAPI.list_files_in_directory(folderReadAPI.find_now_directory()):
+        if(folderReadAPI.extract_fileextension_checker(match)):
+            print(match)
     
 if __name__ == "__main__":
     main()
