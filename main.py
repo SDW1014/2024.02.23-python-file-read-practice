ff6934c518dae8f2e5fa0622274eea759f7aaad5
SDW1014 <apif730@gmail.com>
2024-02-23 23:59:35 +0900

    [feat] main함수 내용 추가. test.txt 추가 - set_file_title 함수의 추가 영역을 사용하는 부분 생성 또 test.txt라는 테스트 요소까지 추가

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import folderReadAPI
 
 def main():
-    # 기본으로 가져가는 hello 함수 #불필요#
+    ## 기본으로 가져가는 hello 함수 #불필요###
     # print("hello")
-    # folderReadAPI.find_now_directory()를 통해 실행 py의 경로를 확인하고, folderReadAPI.list_files_in_directory를 통해 파일들을 리스트로 가져옴 그리고 print함 
-    print(folderReadAPI.list_files_in_directory(folderReadAPI.find_now_directory()))
-    
+    ## folderReadAPI.find_now_directory()를 통해 실행 py의 경로를 확인하고, folderReadAPI.list_files_in_directory를 통해 파일들을 리스트로 가져옴 그리고 print함## 
+    # print(folderReadAPI.list_files_in_directory(folderReadAPI.find_now_directory()))
+    ##test.txt -> demon.txt -> test.txt##
+    folderReadAPI.set_file_title("test.txt", "demon.txt")
+    folderReadAPI.set_file_title("demon.txt", "test.txt")
     
 if __name__ == "__main__":
     main()
